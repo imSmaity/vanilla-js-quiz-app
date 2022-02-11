d19cca58f39c24470d0c575599175a101cf4389d
Suman <60940736+imSmaity@users.noreply.github.com>
2022-02-11 19:18:27 +0530

    new file

---
+++ index.js
@@ -0,0 +1,7 @@
+import {questionAndOptions} from './questionAndOptions.js'
+
+const q1=document.querySelector('#o2')
+
+q1.addEventListener('input',()=>{
+    alert(q1.value)
+})
---
+++ questionAndOptions.js
@@ -0,0 +1,10 @@
+export const questionAndOptions=[
+    {
+        question: 'Which is the first missile in India?',
+        option1: 'V-2 rocket',
+        option2: 'Prithvi',
+        option3: 'Trishul',
+        option4: 'Agni',
+    }
+]
+
