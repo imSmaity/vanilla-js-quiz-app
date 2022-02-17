fdba717b4ea16df8000d30697f51f9ac274caf6d
Suman <60940736+imSmaity@users.noreply.github.com>
2022-02-18 02:01:40 +0530

    answers

---
+++ pages/html/script/page1.js
@@ -0,0 +1,14 @@
+
+
+function firstPageSubmit(){
+    let result=0;
+    for(let i=1;i<=document.querySelectorAll('.question').length;i++){
+
+        if(document.getElementById(`q${i}Ans`).checked){
+            result++
+        }
+        
+    }
+    alert(result)
+    console.log("Qualify to next round: ",result>=7);
+}
